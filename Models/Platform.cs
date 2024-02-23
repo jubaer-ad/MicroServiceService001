b40e28572868692576e6772f208e718a34262e00
jubaer_ad <jubaerad1@gmail.com>
2024-02-23 22:29:21 +0600

    upto profiles for auto mapper

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -5,9 +5,13 @@ namespace PlatformService.Models
     public class Platform
     {
         [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public required string Name { get; set; }
+        [Required]
         public required string Publisher { get; set; }
+        [Required]
         public required string Cost { get; set; }
     }
 }
